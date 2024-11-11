9b1f8fc0758cea56bc43903a172697a28755e01f
LordStoker <tpc00@iesemilidarder.com>
2024-11-11 21:38:48 +0100

    first commit

---
+++ database/migrations/2024_11_11_203619_create_posts_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 255);
+            $table->string('url_clean', 500);
+            $table->text('content');
+            $table->enum('posted', ['yes', 'not']);
+            $table->string('image', 255);
+            $table->foreignId('category_id')->constrained();
+            $table->foreignId('user_id')->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('posts');
+    }
+};
