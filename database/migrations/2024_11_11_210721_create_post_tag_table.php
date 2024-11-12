fa81899782b0b8ab25fd28beec5ed53da5d7ef2f
LordStoker <tpc00@iesemilidarder.com>
2024-11-12 15:19:19 +0100

    coreccion tabla pivote

--- database/migrations/2024_11_11_210721_create_post_tag_table.php
+++ database/migrations/2024_11_11_210721_create_post_tag_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('post_tags', function (Blueprint $table) {
+        Schema::create('post_tag', function (Blueprint $table) {
             $table->id();
             $table->foreignId('tag_id')->constrained();
             $table->foreignId('post_id')->constrained();
@@ -24,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('post_tags');
+        Schema::dropIfExists('post_tag');
     }
 };
--- database/migrations/2024_11_11_211826_create_post_user_table.php
+++ database/migrations/2024_11_11_211826_create_post_user_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('post_users', function (Blueprint $table) {
+        Schema::create('post_user', function (Blueprint $table) {
             $table->id();
             $table->text('content');
             $table->foreignId('user_id')->constrained();
@@ -25,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('post_users');
+        Schema::dropIfExists('post_user');
     }
 };
