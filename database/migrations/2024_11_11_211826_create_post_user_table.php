f27905f0bb04bbecefbdf8398c2d2747f30501df
LordStoker <tpc00@iesemilidarder.com>
2024-11-13 20:01:11 +0100

    context to comment

--- database/migrations/2024_11_11_211826_create_post_user_table.php
+++ database/migrations/2024_11_11_211826_create_post_user_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('post_user', function (Blueprint $table) {
             $table->id();
-            $table->text('content');
+            $table->text('comment');
             $table->foreignId('user_id')->constrained();
             $table->foreignId('post_id')->constrained();
             $table->timestamps();
