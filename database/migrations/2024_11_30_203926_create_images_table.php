cf4bb7d202fbf40687bedc8b47f5940152fc6717
LordStoker <tpc00@iesemilidarder.com>
2024-11-19 18:00:10 +0100

    No need to especify table in constrained

--- database/migrations/2024_11_30_203926_create_images_table.php
+++ database/migrations/2024_11_30_203926_create_images_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('images', function (Blueprint $table) {
             $table->id();
             $table->string('name', 255);
-            $table->foreignId('comment_id')->constrained("comments");
+            $table->foreignId('comment_id')->constrained();
             $table->timestamps();
         });
     }
